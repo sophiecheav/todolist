c4407f1108905c11558822052c5cdcd5386e6e83
sophiecheav <sophie.cheavseang@gmail.com>
2020-03-10 22:22:37 +0100

    fix bug checkboxes / text appearance

--- todolist.js
+++ todolist.js
@@ -1,17 +1,20 @@
-let toDoList = [
+toDoList = [
   "acheter des légumes",
   "finir le dossier de stage",
   "faire les exos de javascript"
 ];
 
-document.querySelector('.add-button').addEventListener('click', function() {
-  document.querySelector('.liste1').innerHTML = "";
-  toDoList.forEach(function(elementToDoList){
-    document.querySelector('.liste1').innerHTML += `<li><input type="checkbox" class="checkbox"><label>${elementToDoList}</label></li>`;
- });
+//comment empêcher que la liste se réécrive à chaque clic sur le bouton 'afficher la liste' ?
+
+document.querySelector('.display-button').addEventListener('click', function() {
+  toDoList.forEach(function(theList) {
+    document.querySelector('.liste1').innerHTML += `<li><input type="checkbox" class="checkbox"><label id="list">${theList}</label></li>`;
+  });
 });
 
-document.querySelector('.add-button').addEventListener('click', function(){
-   toDoList.push(document.querySelector('.champ-list').value);
-   document.querySelector('.champ-list').value = "";
-});
+document.querySelector('.add-button').addEventListener('click', function() {
+  let newTask = document.querySelector('.champ-list').value;
+  toDoList.push(newTask);
+    document.querySelector('.liste1').innerHTML += `<li><input type="checkbox" class="checkbox"><label id="task">${newTask}</label></li>`;
+    document.querySelector('.champ-list').value = '';
+  });
