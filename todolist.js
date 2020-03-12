8d316b43b9a44f12449f5c2d5557f28c48fbff5a
sophiecheav <sophie.cheavseang@gmail.com>
2020-03-12 15:34:15 +0100

    réglé répétition liste
    au clic "afficher la liste"

--- todolist.js
+++ todolist.js
@@ -4,9 +4,12 @@ toDoList = [
   "faire les exos de javascript"
 ];
 
-//comment empêcher que la liste se réécrive à chaque clic sur le bouton 'afficher la liste' ?
+//comment empêcher que la liste se réécrive à chaque clic sur le bouton 'afficher la liste' ?   document.querySelector('.liste1').innerHTML = '';
+
+
 
 document.querySelector('.display-button').addEventListener('click', function() {
+  document.querySelector('.liste1').innerHTML = '';
   toDoList.forEach(function(theList) {
     document.querySelector('.liste1').innerHTML += `<li><input type="checkbox" class="checkbox"><label id="list">${theList}</label></li>`;
   });
